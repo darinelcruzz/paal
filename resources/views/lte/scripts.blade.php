16ba7e59e6bc55e24c05a4478e7df5feb4d50090
Darinel Cruz <darinelcruzz@gmail.com>
2019-02-18 16:43:35 -0600

    Prevent double submitting

--- resources/views/lte/scripts.blade.php
+++ resources/views/lte/scripts.blade.php
@@ -65,6 +65,16 @@
         // submit the form
         btn.form.submit();
     }
+
+    $('form').submit(function (event) {
+        if ($(this).hasClass('submitted')) {
+            event.preventDefault();
+        }
+        else {
+            // $(this).find(':submit').html('<i class="fa fa-spinner fa-spin"></i>');
+            $(this).addClass('submitted');
+        }
+    });
     
     function printTicket() {
         window.print();
