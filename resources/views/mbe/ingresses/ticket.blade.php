0f95c918dbef0d873ab070f7bc47ad6486e12a3c
Darinel Cruz <darinelcruzz@gmail.com>
2019-10-31 11:41:06 -0600

    Needs approval

--- resources/views/mbe/ingresses/ticket.blade.php
+++ resources/views/mbe/ingresses/ticket.blade.php
@@ -39,40 +39,21 @@
         <div class="col-md-6 col-md-offset-3">
             <p>
                 <big><b>SUCURSAL CHS</b></big><br>
-                Blvd Angel Albino Corzo #955<br>
-                Loc A y B COl. Las Palmas CP 29040 <br>
-                <i class="fas fa-phone"></i> 01 (961) 121 34 04 - <i class="fab fa-whatsapp"></i> 961 330 65 28 <br>
-                <i class="fas fa-envelope"></i> ventas@coffeedepotchiapas.com.mx <br>
-                <i class="fab fa-facebook"></i> Coffee Depot TGZ
+                16 Poniente Norte #138 <br>
+                Fracc. Las Arboledas C.P. 29030 <br>
             </p>
         </div>
     </div>
 
     <div class="row">
         <div class="col-md-12">
-            <span class="pull-left">{{ strtoupper($ingress->client->name) }}</span> <br>
-            <span class="pull-left">{{ $ingress->client->rfc }}</span> <br>
-            <span class="pull-left">{{ $ingress->client->email }}</span> <br>
+            <span style="text-align: center;">{{ strtoupper($ingress->client->name) }}</span>
         </div>
     </div>
-
-    <div class="row">
-        <div class="col-md-12">
-            @if ($ingress->invoice == 'otro')
-                <span class="pull-right">Otro:__________________________</span>
-            @elseif($ingress->invoice != 'no')
-                <span class="pull-right">{{ $ingress->cfdi }}</span>
-            @else
-                <span class="pull-right">S/F</span>
-            @endif
-        </div>
-    </div>
-
     <br>
-
     <div class="row">
         <div class="col-md-12">
-            <span class="pull-left"><b>Factura: </b> {{ $ingress->invoice_id }}</span> 
+            <span class="pull-left"><b>FI</b> {{ $ingress->invoice_id }}</span> 
             <span class="pull-right">{{ fdate($ingress->created_at, 'd/m/Y h:i a') }}</span>
         </div>
     </div>
@@ -85,16 +66,16 @@
             <table style="width: 100%;">
                 <thead>
                     <tr>
-                        <th>CANT</th>
                         <th style="width: 55%">DESCRIPCIÓN</th>
+                        <th>CANT</th>
                     </tr>
                 </thead>
 
                 <tbody>
                     @foreach (unserialize($ingress->products) as $product)
                         <tr>
+                            <td style="text-align: center;">{{ App\Product::find($product['i'])->description }}</td>
                             <td style="text-align: center;">{{ $product['q'] }}</td>
-                            <td>{{ App\Product::find($product['i'])->description }}</td>
                         </tr>
                     @endforeach
                 </tbody>
@@ -102,11 +83,11 @@
                 <tfoot>
                     <tr>
                         <th>I.V.A.</th>
-                        <td style="text-align: right">$ {{ number_format($ingress->iva, 2) }}</td>
+                        <td style="text-align: center">$ {{ number_format($ingress->iva, 2) }}</td>
                     </tr>
                     <tr style="border:1px solid black">
                         <th>Total</th>
-                        <td style="text-align: right">$ {{ number_format($ingress->amount, 2) }}</td>
+                        <td style="text-align: center">$ {{ number_format($ingress->amount, 2) }}</td>
                     </tr>
                 </tfoot>
             </table>
