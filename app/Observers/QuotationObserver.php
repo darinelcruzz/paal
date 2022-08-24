18df82b494b68483a43e5d949d13e9199f2e82be
Darinel Cruz <darinelcruzz@gmail.com>
2022-08-24 12:31:04 -0500

    Issue with moving quotations from pre to terminated fixed

--- app/Observers/QuotationObserver.php
+++ app/Observers/QuotationObserver.php
@@ -13,25 +13,27 @@ class QuotationObserver
 
     function updated(Quotation $quotation)
     {
-        if (count($quotation->movements) > 0 && $quotation->getOriginal('status') != 'pendiente') {
-            $products = $quotation->movements->pluck('product_id')->toArray();
-            foreach (request('items') as $index => $item) {
-                if (in_array($item['product_id'], $products)) {
-                    $movement = $quotation->movements->where('product_id', $item['product_id'])->first();
-                    $movement->update($item);
-                } else {
-                    $quotation->movements()->create($item);
+        if($quotation->getOriginal('status') != 'pendiente') {
+            if (count($quotation->movements) > 0) {
+                $products = $quotation->movements->pluck('product_id')->toArray();
+                foreach (request('items') as $index => $item) {
+                    if (in_array($item['product_id'], $products)) {
+                        $movement = $quotation->movements->where('product_id', $item['product_id'])->first();
+                        $movement->update($item);
+                    } else {
+                        $quotation->movements()->create($item);
+                    }
                 }
-            }
 
-            foreach ($quotation->movements as $movement) {
-                $products = collect(request('items'))->pluck('product_id')->toArray();
-                if (!in_array($movement->product_id, $products)) {
-                    $movement->delete();
+                foreach ($quotation->movements as $movement) {
+                    $products = collect(request('items'))->pluck('product_id')->toArray();
+                    if (!in_array($movement->product_id, $products)) {
+                        $movement->delete();
+                    }
                 }
+            } else {
+                $quotation->movements()->createMany(request('items'));
             }
-        } else {
-            $quotation->movements()->createMany(request('items'));
         }
     }
 
